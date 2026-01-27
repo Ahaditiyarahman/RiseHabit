3fb9cc2e39818e4bb60b857cb0f4385c3c3f0fb7
ZikriGlorious5 <zikrizikri026@gmail.com>
2026-01-27 23:06:02 +0700

    Splash Icon Main

--- app/src/main/java/com/example/risehabit/MainActivity.java
+++ app/src/main/java/com/example/risehabit/MainActivity.java
@@ -8,17 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.core.splashscreen.SplashScreen;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // 1. Panggil ini PALING AWAL
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
